3d99158bad7c03332fd9fa5778ce6dbd7a8f3e79
Satya Prakash <satyaprakash@AS-MAC-0234.local>
2022-03-14 09:58:17 +0530

    breaking task

--- dags/task2.py
+++ dags/task2.py
@@ -24,18 +24,18 @@ def create_weather_table():
 
         print("Table Created successfully")
 
-        insert_query = "Insert into weather (STATE, DESCRIPTION, TEMPERATURE, FEELS_LIKE_TEMPERATURE,MIN_TEMP, " \
-                       "MAX_TEMP,HUMIDITY,CLOUDS) values (%s,%s,%s,%s,%s,%s,%s,%s)"
-
-        for index, row in df.iterrows():
-            print("added..")
-            cursor.execute(insert_query, (
-                row['State'], row['Description'], row['Temperature'], row['Feels_Like_Temperature']
-                , row['Min_Temperature'], row['Max_Temperature'], row['Humidity'], row['Clouds']))
-
-        conn.commit()
-
-        print("values are Inserted successfully")
+        # insert_query = "Insert into weather (STATE, DESCRIPTION, TEMPERATURE, FEELS_LIKE_TEMPERATURE,MIN_TEMP, " \
+        #                "MAX_TEMP,HUMIDITY,CLOUDS) values (%s,%s,%s,%s,%s,%s,%s,%s)"
+        #
+        # for index, row in df.iterrows():
+        #     print("added..")
+        #     cursor.execute(insert_query, (
+        #         row['State'], row['Description'], row['Temperature'], row['Feels_Like_Temperature']
+        #         , row['Min_Temperature'], row['Max_Temperature'], row['Humidity'], row['Clouds']))
+        #
+        # conn.commit()
+        #
+        # print("values are Inserted successfully")
 
     except:
         print("Error in connection")
